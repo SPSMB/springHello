a6818c89b872f1bf21819a39effad6c1c2d03984
stemberk <stemberk@spsmb.cz>
2022-09-08 11:00:31 +0200

    popis +

--- src/main/java/cz/spsmb/a4it2/spring/hello/Application.java
+++ src/main/java/cz/spsmb/a4it2/spring/hello/Application.java
@@ -8,18 +8,29 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Felipe Gutierrez
+ * Spring framework je založen na jednoduchém principu: Injekce závislostí Jedná se o návrhový vzor,
+ * který funguje na základě návrhu rozhraní a injektáží přes settery, či konstruktory všech závislostí
+ * a implemantací, které spolupracují a intaragují napříč třídami.
+ *
+ * Výhodou tohoto přístupu je, že pokud změníme implementaci, uděláme to pouze v jedné třídě a ostatní
+ * zůstane nedotčeno. Takto je umožňeno vytvářet robustní a do budoucna rozšiřitelnou aplikaci.
  *
  */
 @Configuration
 @ComponentScan
 public class Application {
 
+    // Touto anotací říkáme kontejneru Springu. že třída HelloWorldMessage bude použita později v
+    // nějakém bodě.
     @Bean
     MessageService helloWorldMessageService() {
         return new HelloWorldMessage();
     }
 
     public static void main(String[] args) {
+        // Třída ApplicationContext slouží k vytvoření Spring kontejneru. Pomocí této třídy s pomocí
+        // dalších anotací (@Configuration, @ComponentScan, @Bean) vytvoříme kontrejner a pospojujeme
+        // vše tak, že metoda getBean Spring kontejneru ví, krerý objekt se má použít.
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(Application.class);
 
